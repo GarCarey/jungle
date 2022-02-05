efc37df6aa1a0f6ad36804777589282a2c2ffa72
Gareth <gar.carey@gmail.com>
2022-02-05 09:45:45 -0500

    add email strip and downcase validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,23 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  before_validation :strip_downcase
+
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 8}
+
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.strip.downcase)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+    end
+  end
+
+  def strip_downcase
+    self.email = self.email.strip.downcase unless self.email.nil?
+  end
 end
